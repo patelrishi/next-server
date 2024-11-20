a43835b05fe5a74a2c2dd31503ca4ebcb691e4dc
patelrishi <patelsai9696@gmail.com>
2024-11-20 20:56:02 +0530

    token implement

---
+++ common/validateToken.js
@@ -0,0 +1,17 @@
+var jwt = require('jsonwebtoken');
+
+function validateToken(req, res, next) {
+    const token = req.headers.authorization
+    if (token) {
+       jwt.verify(token,"apptoken",function(error){
+        if(error){
+          res.send("Invalid token")
+        }else{
+          next()
+        }
+      })
+    } else{
+      res.send("token missing")
+    }
+    }
+module.exports = validateToken
--- routes/student.js
+++ routes/student.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-var getDB = require('../common/dbConnection')
+var getDB = require('../common/dbConnection');
+var jwt = require('jsonwebtoken');
+var validateToken = require('../common/validateToken')
+
 var ObjectId=mongodb.ObjectId;
 
-router.post('/register', async function(req,res,next){
+router.post(
+  '/register',
+  validateToken,
+  async function(req,res,next){
 try {
   const data = req.body.data
   const db = await getDB();
   const collection = db.collection('students')
   const result = await collection.insertOne(data)
-  res.send(result)
-
+  res.send(result);
 } catch(ex){
     res.send(ex.message)
 }
 })
 
-router.get('/get-student', async function(req,res,next){
+router.get('/get-student',
+  validateToken,
+   async function(req,res,next){
   try{
     const db = await getDB();
     const collection = db.collection("students");
@@ -29,7 +36,7 @@ router.get('/get-student', async function(req,res,next){
   }
 })
 
-router.put("/put-student", async function(req,res,next){
+router.put("/put-student",validateToken, async function(req,res,next){
 try{
   const id=req.query.id;
   const data=req.body.data;
@@ -43,7 +50,7 @@ res.send(ex);
 }
 })
 
-router.delete('/delete-student/:id', async function(req,res,next){
+router.delete('/delete-student/:id',validateToken,async function(req,res,next){
  try{  
   var id = req.params.id;
   const db = await getDB()
@@ -56,10 +63,11 @@ router.delete('/delete-student/:id', async function(req,res,next){
  }
 })
 
-router.post("/login", function(req,res,next){
-  const {uid,password}=req.body
-  if(uid=='nit' && password=="nitnit"){
-    res.send([{uid,password}])
+router.post("/login",validateToken, function(req,res,next){
+  const {uid,pwd}=req.body
+  if(uid == 'nit' && pwd == 'nitnit'){
+    const token=jwt.sign({uid,pwd},"apptoken") //impliment Token
+    res.send([{uid,pwd,token}])
   } else {
     res.send([])
   }
